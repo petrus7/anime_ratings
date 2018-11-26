01007fd90f59b2a9035bbe56cf143658938fadcd
Piotr Dankowski <piotr.dankowski@autea.eu>
2018-11-26 13:39:45 +0100

    projhect setup

---
+++ run.py
@@ -0,0 +1,9 @@
+
+
+if __name__ == '__main__':
+    print('ANIME LIST RATING')
+    print('ANIME LIST RATING')
+	print('ANIME LIST RATING')
+	print('ANIME LIST RATING')
+	print('ANIME LIST RATING')
+	print('ANIME LIST RATING')
